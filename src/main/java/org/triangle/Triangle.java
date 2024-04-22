b2a37f780cdbf3532d22be9c260c7b1c015fea98
Bui Cong <buivancng03@gmail.com>
2024-04-22 17:18:40 +0700

    edit triangle class

--- src/main/java/org/triangle/Triangle.java
+++ src/main/java/org/triangle/Triangle.java
@@ -1,6 +1,6 @@
 package org.triangle;
 
-public class triangle {
+public class Triangle {
     public static String checkTriangleType(int side1, int side2, int side3) {
 
         if (side1 <= 0 || side2 <= 0 || side3 <= 0)
@@ -9,12 +9,10 @@ public class triangle {
             return "Not Triangle";
         else if (side1 == side2 && side2 == side3)
             return "Equilateral";
-        else if (side1 == side2 || side1 == side3 || side2 == side3) {
-            if (side1 * side1 + side2 * side2 == side3 * side3 || side1 * side1 + side3 * side3 == side2 * side2 || side2 * side2 + side3 * side3 == side1 * side1)
-                return "Isosceles and Right";
-            else
+        else if (side1 == side2 || side1 == side3 || side2 == side3)
                 return "Isosceles";
-        }
+        else if (side1 * side1 + side2 * side2 == side3 * side3 || side1 * side1 + side3 * side3 == side2 * side2 || side2 * side2 + side3 * side3 == side1 * side1)
+            return "Right Triangle";
         else
             return "Triangle";
     }
